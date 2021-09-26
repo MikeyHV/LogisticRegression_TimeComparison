d5f5dd7a85854c575090092e9f9aa8191fecf423
Michael Villordon <mvillo01@outlook.com>
2021-09-26 00:00:15 -0500

    Update hw5.cpp
    Work on classCost

--- hw5.cpp
+++ hw5.cpp
@@ -14,7 +14,7 @@ vector<double> predict(vector<double> features, vector<double> weights){
     return pls;
 }
 
-vector<double> updateWeights(vector<double> features,vector<int> labels,vector<double> weights,int lr){
+vector<double> updateWeights(vector<double> features,vector<double> labels,vector<double> weights,int lr){
     int obs = labels.size();
 
     vector<double> preds = predict(features, weights);
@@ -23,26 +23,56 @@ vector<double> updateWeights(vector<double> features,vector<int> labels,vector<d
 
 }
 
-int classCost(vector<double> preds, vector<double> labels){
-
+vector<double> classCost(vector<double> features, vector<double> labels){
+    for(int i = 0; i < features.size(); i++){
+        features[i] = log(features[i]) * labels[i] * -1;
+    }
+    return features;
 }
 
-double costFunction(vector<double> features, vector<int>labels, vector<double>weights){
+double costFunction(vector<double> features, vector<double>labels, vector<double>weights){
+    vector<double> cost1 = costFunction(features, labels, weights);
+
+    vector<double> cost2 = cost2Function(features, labels, weights);
+    
+    vector<double> finCost = costDiff(cost1, cost2);
+
+    double cost = sumVec(finCost)/labels.size();
+
+    return cost;
+}
+
+vector<double> cost2Function(vector<double> features, vector<double>labels, vector<double>weights){
     for(int i = 0; i < features.size(); i++){
-        features[]
+        features[i] = log(1 - features[i]) * (1 - labels[i]) * -1;
     }
+    return features;
 }
 
 double sigmoid(int z){
     return 1.0/(1 + exp(-z));
 }
 
-vector<double> train(vector<double> features,vector<int> labels,vector<double> weights,int lr, int iters){
+vector<double> costDiff(vector<double> one, vector<double> two){
+    for(int i = 0; i < one.size(); i++){
+        one[i] = one[i] - two[i];
+    }
+    return(one);
+}
+
+double sumVec(vector<double> one){
+    double sum = 0;
+    for(int i = 0; i < one.size(); i++){
+        one[i] = one[i] + sum;
+    }
+    return(sum);
+}
+
+vector<double> train(vector<double> features,vector<double> labels,vector<double> weights,int lr, int iters){
     vector<double> hist;
 
     for(int i = 0; i < iters; i++){
         weights = updateWeights(features, labels, weights, lr);
-
         double cost = costFunction(features, labels, weights);
         hist.push_back(cost);
     }
