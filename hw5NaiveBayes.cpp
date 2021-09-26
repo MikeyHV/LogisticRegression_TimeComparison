6a14a2cd80870432495087572cff6ae827ba1b45
Michael Villordon <mvillo01@outlook.com>
2021-09-26 17:37:36 -0500

    Changes dunno bad at git
    beginning sruff

--- hw5NaiveBayes.cpp
+++ hw5NaiveBayes.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cmath>
+#include <string>
+using namespace std;
 
+
+int count_survived(vector<double> data){
+    int survived = 0;
+    for(int i = 0; i < data.size(); i++){
+        if(data[0] == 0){
+            survived++;
+        }
+    }
+    return(survived);
+}
+
+double likelihoods(vector< vector<double> > data){
+    double pc = 0;
+    double sx = 0;
+    double age = 0;
+}
